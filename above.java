67da21ecb495c8938ebfe78de37391163db01e08
joy059 <54514678+joy059@users.noreply.github.com>
2019-08-28 22:08:52 +0100

    Add files via upload

---
+++ above.java
@@ -0,0 +1,7 @@
+public class Myclasas{
+    public Static void Main (String [] args){
+        System.out.println("Hello class, Goodmorning");
+        System.out.println("This is my first website");
+        System.out.println("so what do you have to say about it?");
+    }
+}
